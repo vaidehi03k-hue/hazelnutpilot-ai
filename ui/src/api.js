89a4cc1107f663d7f1f8659d71df95a1f8a6b9c3
Vaidehi Kulkarni <vaidehi03k@gmail.com>
2025-08-25 11:45:47 +0530

    Revert "cloud copy for HF Spaces"
    This reverts commit ff8250705976f6a44c45ca6e001f66777c2884d0.

--- ui/src/api.js
+++ ui/src/api.js
@@ -1,3 +1,3 @@
-import axios from "axios";
-const api = axios.create({ baseURL: import.meta.env.VITE_API_BASE_URL || "/api" });
-export default api;
+import axios from 'axios'
+const api = axios.create({ baseURL: 'http://localhost:4000/api' })
+export default api
